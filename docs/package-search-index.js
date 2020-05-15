a830c6c9179958384554d768d4f50416bdc2fced
Benjamin S <cph-bs190@cphbusiness.dk>
2020-05-15 16:48:00 +0200

    Mini rettelse

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"CarportUtil"},{"l":"DBAccess"},{"l":"FunctionLayer"},{"l":"PresentationLayer"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"FunctionLayer"}]
