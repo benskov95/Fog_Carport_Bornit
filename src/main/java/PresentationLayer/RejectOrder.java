add3d287315f73aa27b04e7eb8ffe5ce5bf15dcd
Benjamin S <cph-bs190@cphbusiness.dk>
2020-05-20 14:30:13 +0200

    Ny status: afvist, og et par ændringer på adminsiden og myorder.jsp

--- src/main/java/PresentationLayer/RejectOrder.java
+++ src/main/java/PresentationLayer/RejectOrder.java
@@ -10,15 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 
-public class DeleteOrder extends Command {
+public class RejectOrder extends Command {
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException, SQLException, ClassNotFoundException {
 
         HttpSession session = request.getSession();
 
-        int order_id = Integer.parseInt(request.getParameter("delete"));
+        int order_id = Integer.parseInt(request.getParameter("reject"));
         BomFacade.deleteBom(order_id);
-        OrderFacade.deleteOrder(order_id);
+        OrderFacade.updateStatus(order_id, 5);
 
         session.setAttribute("orderlist", OrderFacade.getAllOrdersByStatusId(1));
 
